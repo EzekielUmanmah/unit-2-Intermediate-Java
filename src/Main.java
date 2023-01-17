381c4d86292e3dfcba15fcde15e9bb3b130b19b9
Ezekiel <umanmah234@gmail.com>
2023-01-17 12:02:45 -0500

    add cupcake variants

--- src/Main.java
+++ src/Main.java
@@ -19,3 +19,15 @@ class Cupcake {
         System.out.println("A basic, generic cupcake, with vanilla frosting");
     }
 }
+
+class RedVelvet extends Cupcake {
+    public void type(){
+        System.out.println("A red velvet based cupcake, with cream cheese frosting.");
+    }
+}
+
+class Chocolate extends Cupcake {
+    public void type(){
+        System.out.println("A chocolate based cupcake, with chocolate frosting.");
+    }
+}
