468626d8dc589bf043773f0fb507aeb7562106fe
Ezekiel <umanmah234@gmail.com>
2023-01-19 17:23:40 -0500

    add Order class

---
+++ src/Order.java
@@ -0,0 +1,99 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Order {
+
+    public Order(ArrayList<Cupcake> cupcakeMenu, ArrayList<Drink> drinkMenu) {
+        System.out.println("Hello customer. Would you like to place an order? (Y or N)");
+
+        Scanner input = new Scanner(System.in);
+        String placeOrder = input.nextLine();
+
+        ArrayList<Object> order = new ArrayList<>();
+
+        if(Character.toLowerCase(placeOrder.charAt(0)) == 'y') {
+            order.add(LocalDate.now());
+            order.add(LocalTime.now());
+            System.out.println("Here is the menu.");
+
+            System.out.println("CUPCAKES:");
+            int itemNumber = 0;
+
+            for (Cupcake cupcake : cupcakeMenu) {
+                itemNumber++;
+                System.out.print(itemNumber + ". ");
+                cupcake.type();
+                System.out.println("Price: $" + cupcake.getPrice());
+                System.out.println();
+            }
+
+            System.out.println("DRINKS:");
+
+            for(Drink drink: drinkMenu){
+                itemNumber++;
+                System.out.print(itemNumber + ". ");
+                drink.type();
+                System.out.println("Price: $" + drink.getPrice());
+                System.out.println();
+            }
+
+            boolean ordering = true;
+
+            while(ordering) {
+                System.out.println("What would you like to order? Please use the number associated with each item to order.");
+                int orderChoice;
+
+                try {
+                    orderChoice = input.nextInt();
+                } catch (InputMismatchException e) {
+                    input.next();
+                    System.out.println("That's not a valid selection. Please enter a number from 1 to 6 to select items.");
+                    continue;
+                }
+
+                input.nextLine();
+
+                if(orderChoice >= 1 && orderChoice <= 3){
+                    order.add(cupcakeMenu.get(orderChoice - 1));
+                    System.out.println("Cupcake item #" + orderChoice + " added to order");
+                } else if (orderChoice >= 4 && orderChoice <=6) {
+                    order.add(drinkMenu.get(orderChoice - 4));
+                    System.out.println("Drink item #" + orderChoice + " added to order");
+                } else {
+                    System.out.println("Sorry, we don’t seem to have that on the menu.");
+
+                    System.out.println("Would you like to continue ordering? (Y/N)");
+                    placeOrder = input.nextLine();
+                    if(placeOrder.toLowerCase().charAt(0) != 'y'){
+                        ordering = false;
+                    }
+                }
+            }
+
+            System.out.println("Date: " + order.get(0));
+            System.out.println("Time: " + order.get(1));
+
+            double subtotal = 0.0;
+
+            for(int i = 2; i < order.size(); i++){
+                for(int c = 0; c < cupcakeMenu.size(); c++){
+                    if(order.get(i).equals(cupcakeMenu.get(c))){
+                        cupcakeMenu.get(c).type();
+                        subtotal += cupcakeMenu.get(c).getPrice();
+                    }
+                    if(order.get(i).equals(drinkMenu.get(c))){
+                        drinkMenu.get(c).type();
+                        subtotal += drinkMenu.get(c).getPrice();
+                    }
+                }
+            }
+            System.out.println("Your subtotal is: $" + subtotal + "\n");
+
+        } else {
+            System.out.println("Have a nice day then.");
+        }
+    }
+}
