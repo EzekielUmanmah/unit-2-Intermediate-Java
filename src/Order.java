af87e2cd958e0af48e016ea9d4c755d1f88d3760
Ezekiel <umanmah234@gmail.com>
2023-01-19 18:21:33 -0500

    add WriteToFile class

--- src/Order.java
+++ src/Order.java
@@ -1,5 +1,7 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
@@ -16,7 +18,7 @@ public class Order {
 
         ArrayList<Object> order = new ArrayList<>();
 
-        if(Character.toLowerCase(placeOrder.charAt(0)) == 'y') {
+        if (Character.toLowerCase(placeOrder.charAt(0)) == 'y') {
             order.add(LocalDate.now());
             order.add(LocalTime.now());
             System.out.println("Here is the menu.");
@@ -34,7 +36,7 @@ public class Order {
 
             System.out.println("DRINKS:");
 
-            for(Drink drink: drinkMenu){
+            for (Drink drink : drinkMenu) {
                 itemNumber++;
                 System.out.print(itemNumber + ". ");
                 drink.type();
@@ -44,7 +46,7 @@ public class Order {
 
             boolean ordering = true;
 
-            while(ordering) {
+            while (ordering) {
                 System.out.println("What would you like to order? Please use the number associated with each item to order.");
                 int orderChoice;
 
@@ -58,10 +60,10 @@ public class Order {
 
                 input.nextLine();
 
-                if(orderChoice >= 1 && orderChoice <= 3){
+                if (orderChoice >= 1 && orderChoice <= 3) {
                     order.add(cupcakeMenu.get(orderChoice - 1));
                     System.out.println("Cupcake item #" + orderChoice + " added to order");
-                } else if (orderChoice >= 4 && orderChoice <=6) {
+                } else if (orderChoice >= 4 && orderChoice <= 6) {
                     order.add(drinkMenu.get(orderChoice - 4));
                     System.out.println("Drink item #" + orderChoice + " added to order");
                 } else {
@@ -69,7 +71,7 @@ public class Order {
 
                     System.out.println("Would you like to continue ordering? (Y/N)");
                     placeOrder = input.nextLine();
-                    if(placeOrder.toLowerCase().charAt(0) != 'y'){
+                    if (placeOrder.toLowerCase().charAt(0) != 'y') {
                         ordering = false;
                     }
                 }
@@ -80,13 +82,13 @@ public class Order {
 
             double subtotal = 0.0;
 
-            for(int i = 2; i < order.size(); i++){
-                for(int c = 0; c < cupcakeMenu.size(); c++){
-                    if(order.get(i).equals(cupcakeMenu.get(c))){
+            for (int i = 2; i < order.size(); i++) {
+                for (int c = 0; c < cupcakeMenu.size(); c++) {
+                    if (order.get(i).equals(cupcakeMenu.get(c))) {
                         cupcakeMenu.get(c).type();
                         subtotal += cupcakeMenu.get(c).getPrice();
                     }
-                    if(order.get(i).equals(drinkMenu.get(c))){
+                    if (order.get(i).equals(drinkMenu.get(c))) {
                         drinkMenu.get(c).type();
                         subtotal += drinkMenu.get(c).getPrice();
                     }
@@ -94,17 +96,21 @@ public class Order {
             }
             System.out.println("Your subtotal is: $" + subtotal + "\n");
 
+            new CreateFile();
+            new WriteToFile(order);
+
         } else {
             System.out.println("Have a nice day then.");
         }
+        input.close();
     }
 }
 
 class CreateFile {
     public CreateFile() {
-        try{
+        try {
             File salesData = new File("salesData.txt");
-            if(salesData.createNewFile()) {
+            if (salesData.createNewFile()) {
                 System.out.println("File created: " + salesData.getName());
             } else {
                 System.out.println("File already exists.");
@@ -112,5 +118,25 @@ class CreateFile {
         } catch (IOException e) {
             System.out.println("An error occurred");
         }
+    }
+}
+
+class WriteToFile {
+    public WriteToFile(ArrayList<Object> order) {
+        try {
+            FileWriter fw = new FileWriter("salesData.txt", true);
+
+            PrintWriter salesWriter = new PrintWriter(fw);
+
+            for (Object o : order) {
+                salesWriter.println(o);
+            }
+
+            salesWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred");
+        }
+
     }
 }
